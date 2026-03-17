4660a8fe2956d01ec6dc8d3d6a4b85338fd293c5
documentation-ai <accounts@documentation.ai>
2026-03-17 17:50:23 +0530

    add user centric script

---
+++ scripts/usercentrics.js
@@ -0,0 +1,6 @@
+var s = document.createElement('script');
+s.id = 'usercentrics-cmp';
+s.src = 'https://web.cmp.usercentrics.eu/ui/loader.js';
+s.dataset.settingsId = 'uJvIO61OKcoD3b';
+s.async = true;
+document.head.appendChild(s);
